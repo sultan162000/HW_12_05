ab8b755aced1b637dd47bfd2d19c9dfaa3a2f458
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-16 15:31:17 +0500

    Готов!

--- HW_12_05/Program.cs
+++ HW_12_05/Program.cs
@@ -27,16 +27,49 @@ namespace HW_12_05
 
             Console.WriteLine();
 
+            ///////////////3
             string chislo = "gdfgdf234dg54gf*23oP42";
-            Regex reg = new Regex(@"[^*+-\/]+");
-            string operationSimvol = reg.Replace(chislo, "");
-            Console.WriteLine(operationSimvol);
+            Regex reg = new Regex(@"\b");
+            string[] textSplit = reg.Split(chislo);
 
-            Regex regxp = new Regex(@"\b");
-
-            string[] sss = regxp.Split(chislo);
-            Console.WriteLine(sss[1]); 
-            
+            reg = new Regex(@"[0-9]+");
+            var matches = reg.Matches(textSplit[1]);
+            string chislo1 = "";
+            foreach (Match m in matches)
+            {
+                chislo1 += m.Value;
+            }
+            string chislo2 = "";
+            var matches1 = reg.Matches(textSplit[3]);
+            foreach (Match item in matches1)
+            {
+                chislo2 += item.Value;
+            }
+            switch (textSplit[2])
+            {
+                case "+":
+                    Console.WriteLine(Convert.ToInt32(chislo1)+Convert.ToInt32(chislo2));
+                    break;
+                case "-":
+                    Console.WriteLine(Convert.ToInt32(chislo1) - Convert.ToInt32(chislo2));
+                    break;
+                case "/":
+                    Console.WriteLine(Convert.ToInt32(chislo1) / Convert.ToInt32(chislo2));
+                    break;
+                case "*":
+                    Console.WriteLine(Convert.ToInt32(chislo1) * Convert.ToInt32(chislo2));
+                    break;
+                default:
+                    Console.WriteLine("Ошибка операции.");
+                    break;
+            }
+            //////////////4
+            string strRegex = @"(?<=[a-z])([A-Z])|(?<=[A-Z])([A-Z][a-z])";
+            reg = new Regex(strRegex, RegexOptions.None);
+            string strString = @"camelCasingText";
+            string strReplace = @" $1$2";
+            var myreg = reg.Replace(strString, strReplace);
+            Console.WriteLine(myreg);
 
             Console.ReadKey();
         }

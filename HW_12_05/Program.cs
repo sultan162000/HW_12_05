1396bb71c8120af9320fe22b8a097a04adcb2f5e
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-16 14:05:59 +0500

    Проблема с 3 пунктам ДЗ, нужно найти регулярный выражение который разделить текст на 3 части

---
+++ HW_12_05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HW_12_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "hello world, aaAaa!";
+            var stext = from p in text
+                        select p.ToString().ToLower().Replace('a', '1').Replace('e', '2').Replace('i', '3').Replace('o', '4').Replace('u', '5');
+            foreach (var item in stext)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            var repText = from p in text
+                          select p.ToString().ToLower().Replace('1', 'a').Replace('2', 'e').Replace('3', 'i').Replace('4', 'o').Replace('5', 'u');
+            foreach (var item in repText)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            string chislo = "gdfgdf234dg54gf*23oP42";
+            Regex reg = new Regex(@"[^*+-\/]+");
+            string operationSimvol = reg.Replace(chislo, "");
+            Console.WriteLine(operationSimvol);
+
+            Regex regxp = new Regex(@"\b");
+
+            string[] sss = regxp.Split(chislo);
+            Console.WriteLine(sss[1]); 
+            
+
+            Console.ReadKey();
+        }
+    }
+}
